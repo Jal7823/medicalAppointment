d7e124a172379970b0e3cc27872299c7a400201b
Jal7823 <jal7823@gmail.com>
2021-11-14 15:35:24 -0300

    OK:labels form appointment

--- appointment/forms.py
+++ appointment/forms.py
@@ -4,14 +4,17 @@ from django.contrib.auth.models import User
 from usersApp.models import Usuario
 
 
-
-
-
 class formsAppointment(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = '__all__'
         widgets = {
-            'date':forms.DateInput(),
+            
+            'date':forms.DateInput(attrs={'type': 'date'})
         }
+        labels = {
+            'specialty':'Especialidad',
+            'date':'Fecha',
+            'branchOffices':'Seleccion la oficina'
 
+        }
