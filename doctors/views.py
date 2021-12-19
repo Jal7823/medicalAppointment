c31597a6e904e1c0ceff5efbc3d5dd28d14bac1b
Jal7823 <jal7823@gmail.com>
2021-12-19 20:00:02 -0300

    ok:ordering icons in index

--- doctors/views.py
+++ doctors/views.py
@@ -8,13 +8,13 @@ from doctors.models import Doctor, Specialty
 
 def index(request):
     branchOffices = BranchOffices.objects.all()[:4]
-    specialty = Specialty.objects.all()
+    specialty = Specialty.objects.all()[:6]
     doctors = Doctor.objects.all()
 
     context = {
-        'branchOffices':branchOffices,
-        'specialty':specialty,
-        'doctors':doctors,
+        'branchOffices': branchOffices,
+        'specialty': specialty,
+        'doctors': doctors,
     }
     return render(request, 'index.html', context)
 
@@ -23,26 +23,21 @@ def error404(request):
     return render(request, 'error404.html')
 
 
-
-
-def detailSpecialty(request,id):
+def detailSpecialty(request, id):
     specialtyQuery = Specialty.objects.get(id=id)
     branchOffices = BranchOffices.objects.filter(
-        Q(specialty__name__icontains = specialtyQuery ),
+        Q(specialty__name__icontains=specialtyQuery),
     )
     context = {
-        'branchOffices':branchOffices,
+        'branchOffices': branchOffices,
     }
 
-    return render(request, 'branchOffices/specialtyFilter.html',context)
+    return render(request, 'branchOffices/specialtyFilter.html', context)
 
 
 def about(request):
     title = 'Sobre nosotros'
     context = {
-        'title':title,
+        'title': title,
     }
-    return render(request, 'about.html',context)
-
-
-
+    return render(request, 'about.html', context)
