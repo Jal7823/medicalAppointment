4e9a7f431f28727674eb9ea4408bca88a9812a35
Jal <70490845+Jal7823@users.noreply.github.com>
2022-02-05 20:22:22 -0300

    ok:  change data base

--- medicalAppointment/settings.py
+++ medicalAppointment/settings.py
@@ -16,7 +16,7 @@ import os.path
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
