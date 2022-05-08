f14c167d35ef69f03a41703f72e572b424ab651b
jal7823 <jal7823@gmail.com>
2022-05-08 16:05:32 -0300

    test of heroku

--- medicalAppointment/settings.py
+++ medicalAppointment/settings.py
@@ -35,8 +35,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('LOCAL_DEBUG')
-ALLOWED_HOSTS = env('LOCAL_HOSTS')
+DEBUG = env('WEB_DEBUG')
+ALLOWED_HOSTS = env('WEB_HOSTS')
 
 
 # User
