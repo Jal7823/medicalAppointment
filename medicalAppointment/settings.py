04bc4e359d9ade21de8ba48d38853fe3ea4c7b89
jal7823 <jal7823@gmail.com>
2022-06-26 18:59:50 -0300

    debug

--- medicalAppointment/settings.py
+++ medicalAppointment/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('WEB_DEBUG')
+DEBUG = False
 ALLOWED_HOSTS = ['medicalappointment.pythonanywhere.com',]
 
 # User
